38d8753eb68d477bb8d162cc2015ab930c15549d
Aman Roy <royaman56456@gmail.com>
2024-12-16 09:15:28 +0530

    Updated Constants and Login design

--- src/components/Login.js
+++ src/components/Login.js
@@ -97,17 +97,18 @@ const Login = () => {
   };
   return (
     <>
-      <div>
+      <div className="relative min-h-screen flex items-center justify-center">
         <Header />
-        <div className="absolute">
-          <img 
+        <div className="absolute inset-0">
+          <img
             src={BG_URL}
-            alt="logo"
+            alt="background"
+            className="w-full h-full object-cover"
           />
         </div>
         <form
           onSubmit={(e) => e.preventDefault()}
-          className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+          className="relative z-10 w-full md:w-3/12 p-12 bg-black text-white rounded-lg bg-opacity-80"
         >
           <h1 className="font-bold text-3xl py-4">
             {isSignInForm ? "Sign In" : "Sign Up"}
@@ -117,27 +118,27 @@ const Login = () => {
               ref={name}
               type="text"
               placeholder="Full Name"
-              className="p-4 my-4 w-full bg-gray-700"
+              className="p-4 my-4 w-full bg-gray-700 rounded-lg"
             />
           )}
-
+  
           <input
             ref={email}
             type="text"
             placeholder="Email Address"
-            className="p-4 my-4 w-full bg-gray-700"
+            className="p-4 my-4 w-full bg-gray-700 rounded-lg"
           />
-
+  
           <input
             ref={password}
             type="password"
             placeholder="Password"
-            className="p-4 my-4 w-full bg-gray-700"
+            className="p-4 my-4 w-full bg-gray-700 rounded-lg"
           />
           <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
-
+  
           <button
-            className="p-4 my-6 bg-red-700 w-full rounded-lg"
+            className="p-4 my-6 bg-red-700 w-full rounded-lg hover:bg-red-800 transition duration-300"
             onClick={handleButtonClick}
           >
             {isSignInForm ? "Sign In" : "Sign Up"}
@@ -145,12 +146,13 @@ const Login = () => {
           <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
             {isSignInForm
               ? "New to Netflix? Sign Up Now"
-              : "Already registered? Signu In Now."}
+              : "Already registered? Sign In Now."}
           </p>
         </form>
       </div>
     </>
   );
+  
 };
 
 export default Login;
